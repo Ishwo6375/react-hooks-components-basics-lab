2d377600637eef91e255a2b7f144c5b62b211ae8
ishwo6375 <ishaan.prazapati6375@gmail.com>
2021-05-13 21:24:45 -0400

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -15,11 +15,21 @@ function Home() {
     </div>
   );
 }
+  function About(){
+    return (
+      <div id="about">
+        <h2>About</h2>
+      </div>
+    )
+  }
+
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Home />
+      <About />
       {/* add the <Home> component here */}
       {/* write an <About> component here */}
     </div>
